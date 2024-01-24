f4e10051acba29c1078242afc31d4ce1fb834366
@ivan_xoxol <ivan_mischenko@niuitmo.ru>
2024-01-25 02:11:42 +0300

    add main func of bot

--- bot_main.py
+++ bot_main.py
@@ -1,9 +1,10 @@
 from aiogram import Bot, Dispatcher, executor, types
 
-import bot_config
-import markups
-import texts
-import database
+import config.bot_config as bot_config
+import resources.markups as markups
+import resources.texts as texts
+import database as db
+import os
 
 bot = Bot(bot_config.BOT_TOKEN)
 dp = Dispatcher(bot)
@@ -11,42 +12,79 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
-    await bot.send_message(message.from_user.id, f"Привет, {message.from_user.first_name}!")
-    await bot.send_message(message.from_user.id, texts.intro, reply_markup=markups.auth)
+    user_id = message.from_user.id
+    db.db_delete_user_data(user_id)
+    db.db_insert_ids(user_id)
+    await bot.send_message(user_id, f"Привет, {message.from_user.first_name}!")
+    await bot.send_message(user_id, texts.intro, reply_markup=markups.auth)
 
 
-flg_verify, flg_login , flg_pass = False
-flg_person, flg_name = False
 @dp.message_handler()
 async def bot_message(message: types.Message):
+    user_id = message.from_user.id
     if (message.chat.type == 'private'):
         if (message.text == 'VERIFY'):
-            flg_verify = True
-            await bot.send_message(message.from_user.id, "Введите Логин:")
-        elif (message.text == bot_config.bot_login and flg_verify):
-            flg_login = True
-            await bot.send_message(message.from_user.id, "Введите Пароль:")
-        elif (message.text == bot_config.bot_password and flg_verify and flg_login):
-            flg_pass = True
-            await bot.send_message(message.from_user.id, 
-                                    "Поздравляем, вы успешно верифицированы!",
+            db.db_set_state("login", user_id)
+            await bot.send_message(user_id, "Введите Логин:")
+        elif (message.text != bot_config.bot_login and db.db_get_state(user_id) == "login"):
+            await bot.send_message(user_id, "Введите Логин:")
+        elif (message.text == bot_config.bot_login and db.db_get_state(user_id) == "login"):
+            db.db_set_state("pass", user_id)
+            await bot.send_message(user_id, "Введите Пароль:")
+        elif (message.text != bot_config.bot_password and db.db_get_state(user_id) == "pass"):
+            await bot.send_message(user_id, "Введите Пароль:")
+        elif (message.text == bot_config.bot_password and db.db_get_state(user_id) == "pass"):
+            db.db_set_state("person", user_id)
+            db.db_set_auth("done", user_id)
+            await bot.send_message(user_id, "Поздравляем, вы успешно верифицированы!", 
                                     reply_markup=markups.person)
-        elif (flg_pass): # auth done
+        elif (db.db_get_auth(user_id) == "done"):
             if (message.text == 'Добавить ФИО'):
-                flg_person = True
-                await bot.send_message(message.from_user.id, "Введите Имя:")
-            elif (flg_person):
-                flg_name = True
-                await bot.send_message(message.from_user.id, "Введите Фамилию:")
-            elif (flg_person and flg_name):
-                flg_person, flg_name = False
-                await bot.send_message(message.from_user.id, 
-                                        "Рады знакомству!",
+                db.db_set_state("name", user_id)
+                await bot.send_message(user_id, "Введите Имя:")
+            elif (db.db_get_state(user_id) == "name"):
+                db.db_set_name(message.text, user_id)
+                db.db_set_state("surname", user_id)
+                await bot.send_message(user_id, "Введите Фамилию:")
+            elif (db.db_get_state(user_id) == "surname"):
+                db.db_set_surname(message.text, user_id)
+                db.db_set_state("bio", user_id)
+                data = db.db_get_names(user_id)
+                await bot.send_message(user_id, f"Спасибо, {data[0]} {data[1]}, рады знакомству!",
                                         reply_markup=markups.bio)
-            if (message.text == 'Добавить ФОТО'):
-                await bot.send_message(message.from_user.id, texts.photo)
+            elif (message.text == 'Добавить ФОТО'):
+                await bot.send_message(user_id, texts.photo)
+            elif (message.text == 'Узнать статус'):
+                status_enter = db.db_get_status(user_id)
+                if status_enter == 'in':
+                    await bot.send_message(user_id, 'В офисе.')
+                else:
+                    await bot.send_message(user_id, 'Покинул офис.')
+            else:
+                await bot.send_message(user_id, "Не понимаю")
         else:
-            await bot.send_message(message.from_user.id, "Попробуйте снова (VERIFY):")
+            await bot.send_message(user_id, "Попробуйте снова (VERIFY):")
+
+
+@dp.message_handler(content_types=['photo'])
+async def photo_handler(message: types.Message):
+    user_id = message.from_user.id
+    if (db.db_get_state(user_id) != "status") :
+        photo_path = f"images/photo{user_id}.jpg"
+        photo = message.photo[-1]
+        await bot.download_file_by_id(photo.file_id, photo_path)
+        db.db_add_photo(photo_path, user_id)
+        db.db_set_state("status", user_id)
+        os.remove(photo_path)
+        data = db.db_get_names(user_id)
+        await bot.send_message(user_id, 
+                                f"Спасибо, {data[0]} {data[1]}. Ваше фото добавлено.",
+                                reply_markup=markups.status)
+    else:
+        data = db.db_get_names(user_id)
+        await bot.send_message(user_id, 
+            f"{data[0]}, ваше фото уже добавлено. Чтобы поменять его, вам необходимо обратиться к начальству.")
+
 
 if (__name__ == '__main__'):
     executor.start_polling(dp, skip_updates = True)
